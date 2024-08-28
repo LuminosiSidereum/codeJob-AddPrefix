ac908077a2f1faf07d116d694e067e3a0426c5ef
Schulti <phillipp-schultheiss@web.de>
2024-08-28 09:34:42 +0200

    Add function mode_selection

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import os
 import sys
 from pathlib import Path
 
+def mode_selection() -> str:
+    """
+    Prompts the user to select a mode.
+    Returns:
+    str: The selected mode.
+    """
+    mode_selection: bool = True
+    while mode_selection:
+        mode: str = input("Modus auswählen: 0 für Präfix einfügen; 1 für Präfix ändern: ")
+        if mode in ["0", "1"]:
+            mode_selection = False
+        else:
+            print("Ungültige Eingabe!")
+    return mode
 
 
 
